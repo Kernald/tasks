e0c630b060fe0ebfc87b7305110aa455b3bc95ab
Marc Plano-Lesay <marc.planolesay@gmail.com>
2013-02-26 19:40:41 +0100

    Log handler: printf only in debug

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,18 @@
 
 using namespace bb::cascades;
 
+#ifdef QT_DEBUG
 void standardOutput(QtMsgType type, const char* msg) {
 	fprintf(stdout, "%s\n", msg);
 	fflush(stdout);
 }
+#endif // QT_DEBUG
 
 Q_DECL_EXPORT int main(int argc, char **argv) {
     Application app(argc, argv);
+#ifdef QT_DEBUG
     qInstallMsgHandler(standardOutput);
+#endif // QT_DEBUG
 
     // localization support
     QTranslator translator;
